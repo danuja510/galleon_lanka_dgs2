215ae4ca25053d46e2e5a8bf26706527ebce795c
danuja <danuja.nimsara@gmail.com>
2020-02-04 13:58:50 +0530

    modified addCustomer

--- addCustomer.php
+++ addCustomer.php
@@ -1,18 +1,48 @@
 <?php session_start(); ?>
 <<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang='en' dir='ltr'>
   <head>
-    <meta charset="utf-8">
+    <meta charset='utf-8'>
     <title>AddCustomer</title>
   </head>
   <body>
-    <form class="" action="index.html" method="post">
-      <label for="txtName">Name</label>
-      <input type="text" name="txtName" id="txtName">
-      <label for="txtAddress">Address</label>
-      <input type="text" name="txtAddress" id="txtAddress">
-      <label for="txtTPNo">TP No</label>
-      <input type="text" name="txtTPNo" id="txtTPNo">
+    <form class='' action='index.html' method='post'>
+      <table>
+        <tr>
+          <td>
+            <label for='txtName'>Name</label>
+          </td>
+          <td>
+            <input type='text' name='txtName' id='txtName'>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for='txtAddress'>Address</label>
+          </td>
+          <td>
+            <input type='text' name='txtAddress' id='txtAddress'>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for='txtTPNo'>TP No</label>
+          </td>
+          <td>
+            <input type='text' name='txtTPNo' id='txtTPNo'>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for='txtType'>Type</label>
+          </td>
+          <td>
+            <select name='txtType' id='txtType'>
+              <option value=""></option>
+            </select>
+          </td>
+        </tr>
+      </table>
     </form>
 
   </body>
