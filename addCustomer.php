d4e0fe3bc9603abab39b3df29e2ea074d4d85ebc
danuja <danuja.nimsara@gmail.com>
2020-02-04 13:43:48 +0530

    started addCustomer file

---
+++ addCustomer.php
@@ -0,0 +1,20 @@
+<?php session_start(); ?>
+<<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>AddCustomer</title>
+  </head>
+  <body>
+    <form class="" action="index.html" method="post">
+      <label for="txtName">Name</label>
+      <input type="text" name="txtName" id="txtName">
+      <label for="txtAddress">Address</label>
+      <input type="text" name="txtAddress" id="txtAddress">
+      <label for="txtTPNo">TP No</label>
+      <input type="text" name="txtTPNo" id="txtTPNo">
+    </form>
+
+  </body>
+</html>
+<!--dan-->
