18dbb35eaea535b341c2b511027106729de1f887
danuja <danuja.nimsara@gmail.com>
2020-02-04 22:58:44 +0530

    altered dbscript and created purchaseOrders,empHome and signFiles

---
+++ createPurchaseOrders.php
@@ -0,0 +1,12 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>createPurchaseOrders</title>
+  </head>
+  <body>
+
+  </body>
+</html>
+<!--dan-->
