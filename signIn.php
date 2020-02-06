d9fe3524d28f08ad6b816d4174f244bd7a5e1913
danuja <danuja.nimsara@gmail.com>
2020-02-06 18:27:06 +0530

    completed signIn

--- signIn.php
+++ signIn.php
@@ -4,6 +4,38 @@
   <head>
     <meta charset="utf-8">
     <title>signIn</title>
+    <script type="text/javascript">
+    function validateEmpNo()
+    {
+      var eno= document.getElementById("txtENO").value;
+       if (isNaN(eno)) {
+        alert("Enter A Valid Employee Number");
+        return false;
+      }
+      else
+        return true;
+    }
+
+    /*function validatePassword()
+    {
+      var pwd=document.getElementById("txtPass").value;
+      if(pwd=="")
+      {
+        alert("Enter the password");
+        return false;
+      }
+      else
+        return true;
+    }*/
+
+    function validate()
+    {
+      if(validateEmpNo() /*&& validatePassword()*/)
+        {}
+      else
+        event.preventDefault();
+    }
+    </script>
   </head>
   <body>
     <h1>Sign In</h1>
@@ -14,7 +46,7 @@
             <label for="txtENO">Employee No</label>
           </td>
           <td>
-            <input type="text" name="txtENO" id="txtENO">
+            <input type="text" name="txtENO" id="txtENO" required>
           </td>
         </tr>
         <tr>
@@ -22,18 +54,38 @@
             <label for="txtPass">Password</label>
           </td>
           <td>
-            <input type="password" name="txtPass" id="txtPass">
+            <input type="password" name="txtPass" id="txtPass" required>
           </td>
         </tr>
         <tr>
           <td></td>
           <td>
-            <input type="submit" name="btnsubmit" id="btnsubmit" value="Submit">
+            <input type="submit" name="btnsubmit" id="btnsubmit" value="Submit" onclick="validate()">
             <input type="reset" value="Reset" name="btnreset>"
           </td>
         </tr>
       </table>
     </form>
+    <?php
+	if(isset($_POST['btnsubmit'])){
+		$eno=$_POST['txtENO'];
+		$pass=$_POST['txtPass'];
+		$sql="SELECT * FROM `employees` WHERE `eno` = ".$eno." AND `password` LIKE '".$pass."';";
+		$con = mysqli_connect("localhost","root","","galleon_lanka");
+			if(!$con)
+			{
+				die("Error while connecting to database");
+			}
+			$result= mysqli_query($con,$sql);
+			mysqli_close($con);
+	if(mysqli_num_rows($result)>0){
+		$_SESSION['eno']=$eno;
+		header('Location:empHome.php');
+		}else{
+			echo "invalid credentials";}
+		}
+
+	?>
   </body>
 </html>
 <!--dan-->
