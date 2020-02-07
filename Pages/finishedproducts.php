f88d0ac0c2776452008471028c38ad4edfcbdafb
gima <gimantha.k98@gmail.com>
2020-02-07 11:40:04 +0530

    coded php for FP

--- Pages/finishedproducts.php
+++ Pages/finishedproducts.php
@@ -1,9 +1,16 @@
+<?php
+ session_start();
+ if(!isset($_SESSION['eno'])){
+   header('Location:signIn.php');
+ }
+?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Finished products</title>
   </head>
   <body>
+    <h1>add finished products</h1>
     <form class="" action="finishedproducts.php" method="post">
       <table>
         <tr>
@@ -16,12 +23,30 @@
           <td>
             <select name="lstBom" id="lstBom">
               <option value='-----'>-----</option>
-            
+              <?php
+                $sql="SELECT * FROM `bom`;";
+                $con = mysqli_connect("localhost","root","","galleon_lanka");
+                if(!$con)
+                {
+                  die("Error while connecting to database");
+                }
+                $rowSQL= mysqli_query( $con,$sql);
+      					while($row=mysqli_fetch_assoc( $rowSQL )){
+                  echo "
+                    <option value='".$row['bom_id']."'> ".$row['bom_id']." </option>
+                  ";
+                }
+                mysqli_close($con);
+               ?>
             </select>
           </td>
-
+        </tr>
+        <tr>
+          <td colspan="2"><input type="submit" name="btnSubmit" value="submit"></td>
         </tr>
       </table>
     </form>
   </body>
 </html>
+
+<!--gima-->
