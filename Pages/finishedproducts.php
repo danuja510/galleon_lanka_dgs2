c1032c3f59244f780fc97ec5c669d8b06303bd14
gima <gimantha.k98@gmail.com>
2020-02-07 11:24:11 +0530

    completed html structure

---
+++ Pages/finishedproducts.php
@@ -0,0 +1,27 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Finished products</title>
+  </head>
+  <body>
+    <form class="" action="finishedproducts.php" method="post">
+      <table>
+        <tr>
+          <td><label for="txtName">Name</label></td>
+          <td><input type="text" name="txtName" id="txtName" value=""></td>
+        </tr>
+
+        <tr>
+          <td><label for="lstBom">BOM ID</label></td>
+          <td>
+            <select name="lstBom" id="lstBom">
+              <option value='-----'>-----</option>
+            
+            </select>
+          </td>
+
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
