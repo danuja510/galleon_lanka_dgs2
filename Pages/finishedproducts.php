6fd7a2280f21709352ec21208499d9473b7a08e6
gima <gimantha.k98@gmail.com>
2020-02-07 13:04:33 +0530

    completed finished products file

--- Pages/finishedproducts.php
+++ Pages/finishedproducts.php
@@ -8,6 +8,40 @@
   <head>
     <meta charset="utf-8">
     <title>Finished products</title>
+    <script type="text/javascript">
+        function validateBom(){
+        {
+          if(document.getElementById('lstBom').value=="-----")
+          {
+              alert("please select a BOM");
+              return false;
+          }
+          else
+              return true;
+        }
+      }
+
+      function validateValue(){
+        var v=document.getElementById('txtValue').value;
+        if(!isNaN(v)){
+          return true;
+        }
+        else {
+          alert("enter a valid value");
+          return false;
+        }
+      }
+
+      function Validate(){
+        if(validateBom() && validateValue()){
+          alert("finished product added");
+        }
+        else {
+          event.preventDefault();
+        }
+
+      }
+    </script>
   </head>
   <body>
     <h1>add finished products</h1>
@@ -15,7 +49,7 @@
       <table>
         <tr>
           <td><label for="txtName">Name</label></td>
-          <td><input type="text" name="txtName" id="txtName" value=""></td>
+          <td><input type="text" name="txtName" id="txtName" value="" required></td>
         </tr>
 
         <tr>
@@ -42,9 +76,29 @@
           </td>
         </tr>
         <tr>
-          <td colspan="2"><input type="submit" name="btnNext" value="Next"></td>
+          <td><label for="txtValue">Value</label></td>
+          <td><input type="text" name="txtValue" id="txtValue" value=""></td>
+        </tr>
+        <tr>
+          <td colspan="2"><input type="submit" name="btnSubmit" id="btnSubmit" value="Add" onclick="Validate()"></td>
         </tr>
       </table>
+
+      <?php
+  		if(isset($_POST["btnSubmit"])){
+  			$name=$_POST["txtName"];
+        $bom=$_POST["lstBom"];
+        $val=$_POST["txtValue"];
+        $con1=mysqli_connect("localhost","root","","galleon_lanka");
+  			if(!$con1){
+  				die("Cannot connect to DB server");
+  			}
+  			$sql1="INSERT INTO `finished_products` (`Name`, `bom_id`, `value`) VALUES ('".$name."', '".$bom."', '".$val."')";
+  			mysqli_query($con1,$sql1);
+  			mysqli_close($con1);
+  			}
+  	?>
+
     </form>
   </body>
 </html>
