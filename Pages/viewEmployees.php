ef8e245a355c785ca91a686df04a4182c9f11f3a
gima <gimantha.k98@gmail.com>
2020-02-25 18:56:53 +0530

    completed viewEmployees file

---
+++ Pages/viewEmployees.php
@@ -0,0 +1,71 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['eno'])){
+      header('Location:signIn.php');
+    }
+?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>view employees</title>
+  </head>
+  <body>
+    <table>
+      <thead>
+        <th>
+            eno
+        </th>
+        <th>
+            Name
+        </th>
+        <th>
+            Designation
+        </th>
+        <th>
+            Dept
+        </th>
+        <th>
+            Password
+        </th>
+        <th>
+            status
+        </th>
+      </thead>
+
+      <?php
+        $con = mysqli_connect("localhost","root","","galleon_lanka");
+          if(!$con)
+            {
+             die("cannot connect to DB server");
+            }
+           $sql="SELECT * FROM `employees` WHERE `status`='active';";
+           $rowSQL= mysqli_query( $con,$sql);
+           $row = mysqli_fetch_array( $rowSQL );
+        echo "
+        <tr>
+          <td>
+              ".$row['eno']."
+          </td>
+          <td>
+              ".$row['Name']."
+          </td>
+          <td>
+              ".$row['Designation']."
+          </td>
+          <td>
+              ".$row['Dept']."
+          </td>
+          <td>
+              ".$row['password']."
+          </td>
+          <td>
+              ".$row['status']."
+          </td>
+        </tr>
+      </table>
+        ";
+      ?>
+
+  </body>
+</html>
+<!--gima-->
