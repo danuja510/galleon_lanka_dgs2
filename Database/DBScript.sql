25b0cd8e1eb0194d1fa3f5a959696246d94d95c0
gima <gimantha.k98@gmail.com>
2020-03-08 19:11:46 +0530

    altered fp table

--- Database/DBScript.sql
+++ Database/DBScript.sql
@@ -245,3 +245,6 @@ ALTER TABLE `creditors` CHANGE `no` `no` INT(11) NOT NULL AUTO_INCREMENT;
 
 #altered bom table
 ALTER TABLE `bom` CHANGE `mid` `mName` VARCHAR(50) NOT NULL;
+
+#altered finished products table
+ALTER TABLE `finished_products` ADD `status` VARCHAR(20) NULL AFTER `value`;
