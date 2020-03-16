9a90bceda88b37286a5d4d010a79dc8f96b94cfb
gima <gimantha.k98@gmail.com>
2020-03-16 15:04:08 +0530

    completed view stocks file

--- Pages/viewStocks.php
+++ Pages/viewStocks.php
@@ -38,24 +38,44 @@ $dep=$_SESSION['DEPT'];
         <td>item no</td>
         <td>qty</td>
       </thead>
-    
-      <tr>
-        <td>store</td>
-        <td></td>
-        <td></td>
-      </tr>
 
-      <tr>
-        <td>p Floor</td>
-        <td></td>
-        <td></td>
-      </tr>
+      <?php
+      $con=mysqli_connect("localhost","root","","galleon_lanka");
+      if(!$con)
+        {
+          die("cannot connect to DB server");
+        }
+      if($dep=="Manager")
+      {
+      $sql="SELECT dept,item_no,SUM(qty) as finalstock FROM `stocks` GROUP BY dept,item_no;";
+      }
 
+      if($dep=="store")
+      {
+      $sql="SELECT dept,item_no,SUM(qty) as finalstock FROM `stocks` WHERE `dept`='store' GROUP BY dept,item_no;";
+      }
+
+      if($dep=="pfloor")
+      {
+      $sql="SELECT dept,item_no,SUM(qty) as finalstock FROM `stocks` WHERE `dept`='pfloor' GROUP BY dept,item_no;";
+      }
+
+      if($dep=="fGoods")
+      {
+      $sql="SELECT dept,item_no,SUM(qty) as finalstock FROM `stocks` WHERE `dept`='fGoods' GROUP BY dept,item_no;";
+      }
+      $rowSQL= mysqli_query($con,$sql);
+      while($row=mysqli_fetch_assoc($rowSQL))
+      {
+echo"
       <tr>
-        <td>F goods</td>
-        <td></td>
-        <td></td>
+        <td>".$row['dept']."</td>
+        <td>".$row['item_no']."</td>
+        <td>".$row['finalstock']."</td>
       </tr>
+";
+      }
+    ?>
     </table>
 
   </body>
