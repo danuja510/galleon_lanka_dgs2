f5e1f58e85ed8b4910f4321932bba083e782d0d9
gima <gimantha.k98@gmail.com>
2020-03-30 11:27:01 +0530

    started manage interfaces

--- Pages/manageEmployees.php
+++ Pages/manageEmployees.php
@@ -11,6 +11,13 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../Resources/CSS/normalize.css">
+    <link rel="stylesheet" type="text/css" href="../Resources/CSS/grid.css">
+    <link rel="stylesheet" type="text/css" href="../Resources/CSS/ionicons.min.css">
+    <link rel="stylesheet" type="text/css" href="../StyleSheets/MainStyles.css">
+    <link rel="stylesheet" type="text/css" href="../StyleSheets/ManageStyles.css">
+    <link rel="stylesheet" type="text/css" href="../StyleSheets/Select2Styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400&display=swap" rel="stylesheet">
     <title>Manage employees</title>
 
     <script type="text/javascript">
@@ -32,6 +39,24 @@
   </head>
 
   <body>
+  <header>
+        <div class="row">
+            <h1>Manufacturing Management System</h1>
+            <h3>Galleon Lanka PLC</h3>
+        </div>
+        <div class="nav">
+            <div class="row">
+                <div class="btn-navi"><i class="ion-navicon-round"></i></div>
+                <a href="empHome.php">
+                    <div class="btn-home"><i class="ion-home"></i><p>Home</p></div>
+                </a>
+                <a href="logout.php">
+                    <div class="btn-logout"><i class="ion-log-out"></i><p>Logout</p></div>
+                </a>
+                <a href="#"><div class="btn-account"><i class="ion-ios-person"></i><p>Account</p></div></a>
+            </div>
+        </div>
+    </header>
     <?php
       $con = mysqli_connect("localhost","root","","galleon_lanka");
         if(!$con)
@@ -44,6 +69,7 @@
          $row = mysqli_fetch_assoc( $rowSQL);
     echo"
         <form action=\"manageEmployees.php\" method=\"post\">
+        
         <table>
 
         <tr>
@@ -86,6 +112,7 @@
           <td>
             <label for='lstDepartment'>Department</label>
           </td>
+          
           <td>
              <select name=\"lstDepartment\" id=\"lstDepartment\">
 
@@ -135,10 +162,13 @@
             <input type=\"text\" name=\"txtStatus\" id=\"txtStatus\" value=" .$row['status']. " required readonly >
           </td>
         </tr>
+        </table>
 
-        <tr>
-          <td>
-            <input type=\"submit\" name=\"btnUpdate\" id=\"btnUpdate\" value=\"update\" onclick='validateEmail()'>
+        <div class='row'>
+        <div class='row'>
+            <div class='col span-1-of-2'>&nbsp;</div>
+            <div class='col span-1-of-2'>
+            <input type=\"submit\" name=\"btnUpdate\" id=\"btnUpdate\" value=\"Update\" onclick='validateEmail()'>
           </td>
           ";
           $st=$row['status'];
@@ -154,7 +184,10 @@
 
           if($st=='active'){
             echo"
-              <input type='submit' name='btnDelete' id='btnDelete' value='delete'>
+                <input type='submit' name='btnDelete' id='btnDelete' value='Delete'>
+                </div>
+              </div>
+          </div>
               ";
           }
 
@@ -163,7 +196,7 @@
 
                 </td>
             </tr>
-          </table>
+          
      </form>
 
      <?php
@@ -212,6 +245,10 @@
 
         }
         ?>
+        <footer>
+        <div class="row"><p> Copyright &copy; 2020 by Galleon Lanka PLC. All rights reserved.</p></div>
+        <div class="row"><p>Designed and Developed by DGS2</p></div>
+    </footer>
   </body>
 </html>
 
