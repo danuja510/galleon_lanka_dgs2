458bc3ffd38ceb46eec1390315b462ba7c743f63
sithara <sitharasewwandi52@gmail.com>
2020-04-14 18:46:11 +0530

    completed Viewcashreceipt

--- Pages/viewCashreceipt.php
+++ Pages/viewCashreceipt.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['eno'])){
+      header('Location:signIn.php');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,3 +40,51 @@
           date
         </th>
     </tr>
+
+    <?php
+         $con=mysqli_connect("localhost","root","","galleon_lanka");
+         if(!$con)
+           {
+             die("cannot connect to DB server");
+           }
+
+           $sql="SELECT * FROM `cash_receipts`";
+           $rowSQL=mysqli_query($con,$sql);
+           while($row=mysqli_fetch_array($rowSQL))
+           {
+   echo"
+       <tr>
+           <td>
+               ".$row['cr_no']."
+           </td>
+           <td>
+               ".$row['invoice_no']."
+           </td>
+           <td>
+               ".$row['cno']."
+           </td>
+           <td>
+               ".$row['amout']."
+           </td>
+           <td>
+               ".$row['prepared_by']."
+           </td>
+           <td>
+               ".$row['approved_by']."
+           </td>
+           <td>
+               ".$row['remarks']."
+           </td>
+           <td>
+               ".$row['date']."
+           </td>
+       </tr>
+   ";
+           }
+       ?>
+       </table>
+       </form>
+
+   </body>
+   </html>
+   <!--sithara-->
