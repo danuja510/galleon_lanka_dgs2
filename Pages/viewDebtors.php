0c44530c9d56ff355387e26c7413dcc0000df4ca
sithara <sitharasewwandi52@gmail.com>
2020-04-14 18:49:52 +0530

    add html for ViewDeptors

---
+++ Pages/viewDebtors.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>view debtors</title>
+</head>
+<body>
+    <form action="viewDebtors.php">
+    <table>
+    <tr>
+        <th>
+          cno
+        </th>
+        <th>
+          total amount
+        </th>
+        <th>
+          date
+        </th>
+    </tr>
