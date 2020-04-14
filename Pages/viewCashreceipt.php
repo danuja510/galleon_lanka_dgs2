844c23f7039c111225b352354fe71924afeac02d
sithara <sitharasewwandi52@gmail.com>
2020-04-14 18:43:20 +0530

    add html for Viewcr

---
+++ Pages/viewCashreceipt.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>view cash receipt</title>
+</head>
+<body>
+    <form action="viewCashreceipt.php">
+    <table>
+    <tr>
+        <th>
+          cr_no
+        </th>
+        <th>
+          invoice no.
+        </th>
+        <th>
+          c no.
+        </th>
+        <th>
+          amount
+        </th>
+        <th>
+          prepared by
+        </th>
+        <th>
+          approved by
+        </th>
+        <th>
+          remarks
+        </th>
+        <th>
+          date
+        </th>
+    </tr>
