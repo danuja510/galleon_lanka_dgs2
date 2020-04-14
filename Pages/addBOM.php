4da61bdffe3258189ac569194bd2515f9bef36ff
danuja <danuja.nimsara@gmail.com>
2020-04-15 03:57:23 +0530

    created addBOM functions

---
+++ Pages/addBOM.php
@@ -0,0 +1,82 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['eno'])){
+    header('Location:signIn.php');
+  }
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>addBOM</title>
+  </head>
+  <body>
+    <form action="addBOM.php" method="post">
+      <label for="txtName">BOM Name</label>
+      <input type="text" name="txtName" list="lstBOM" required><br>
+      <datalist id="lstBOM">
+        <?php
+          $con=mysqli_connect("localhost","root","","galleon_lanka");
+          if(!$con){
+            die("Cannot connect to DB server");
+          }
+          $sql="SELECT DISTINCT mName FROM `bom`";
+          $rowSQL= mysqli_query( $con,$sql);
+          while($row=mysqli_fetch_assoc( $rowSQL )){
+            echo "<option value='".$row["mName"]."'>";
+          }
+          mysqli_close($con);
+        ?>
+      </datalist>
+      <label for="txtQty">Qty.</label>
+      <input type="number" name="txtQty" min=0 required>
+      <input type="submit" name="btnSubmit" value="Next">
+      <button type="submit" name="btnNext">Add Another Material</button>
+    </form>
+    <?php
+      if (isset($_POST['btnNext'])) {
+        if (isset($_SESSION["bom"])) {
+          $found=FALSE;
+          for ($i=0; $i <sizeof($_SESSION["bom"]) ; $i++) {
+            $bom=explode(',',$_SESSION["bom"][$i]);
+            if ($bom[0]==$_POST['txtName']) {
+              $_SESSION["bom"][$i]="".$_POST['txtName'].",".($bom[1]+$_POST['txtQty'])."";
+              $found=TRUE;
+              header('Location:addBOM.php');
+            }
+          }
+          if (!$found==TRUE) {
+            $_SESSION["bom"][sizeof($_SESSION["bom"])]=$_POST['txtName'].",".$_POST['txtQty'];
+            header('Location:addBOM.php');
+          }
+        }else{
+          $_SESSION["bom"]=[];
+          $_SESSION["bom"][0]=$_POST['txtName'].",".$_POST['txtQty'];
+          header('Location:addBOM.php');
+        }
+      }
+
+      if (isset($_POST['btnSubmit'])) {
+        if (isset($_SESSION["bom"])) {
+          $found=FALSE;
+          for ($i=0; $i <sizeof($_SESSION["bom"]) ; $i++) {
+            $bom=explode(',',$_SESSION["bom"][$i]);
+            if ($bom[0]==$_POST['txtName']) {
+              $_SESSION["bom"][$i]="".$bom[0].",".($bom[1]+$_POST['txtQty'])."";
+              $found=TRUE;
+            }
+          }
+          if (!$found==TRUE) {
+            $_SESSION["bom"][sizeof($_SESSION["bom"])]=$_POST['txtName'].",".$_POST['txtQty'];
+          }
+        }else{
+          $_SESSION["bom"]=[];
+          $_SESSION["bom"][0]=$_POST['txtName'].",".$_POST['txtQty'];
+
+        }
+        header('Location:confirmBOM.php');
+      }
+    ?>
+  </body>
+</html>
+<!--dan-->
---
+++ Pages/confirmBOM.php
@@ -0,0 +1,60 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['eno'])){
+    header('Location:signIn.php');
+  }elseif (!isset($_SESSION['bom'])) {
+    header('Location:addBOM.php');
+  }
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>confirmBOM</title>
+  </head>
+  <body>
+    <table>
+      <thead>
+        <th>Material Name</th>
+        <th>Quantity</th>
+      </thead>
+      <?php
+        for ($i=0; $i <sizeof($_SESSION["bom"]) ; $i++) {
+          $bom=explode(',',$_SESSION["bom"][$i]);
+          echo "
+          <tr>
+            <td>".$bom[0]."</td>
+            <td>".$bom[1]."</td>
+          </tr>
+          ";
+        }
+      ?>
+      <tr>
+        <td class="bt">&nbsp;</td>
+        <form action="confirmBOM.php" method="post">
+          <td class="bt"><input type='submit' value='Confirm' name='btnConfirm'></td>
+        </form>
+      </tr>
+    </table>
+    <?php
+    if (isset($_POST['btnConfirm'])) {
+      $con=mysqli_connect("localhost","root","","galleon_lanka");
+      if(!$con){
+        die("Cannot connect to DB server");
+      }
+      $rowSQL = mysqli_query( $con,"SELECT MAX( bom_id ) AS max FROM `bom`;" );
+      $row = mysqli_fetch_array( $rowSQL );
+      $max = $row['max'];
+      $bom_id=$max+1;
+      for ($i=0; $i <sizeof($_SESSION["bom"]) ; $i++) {
+        $bom=explode(',',$_SESSION["bom"][$i]);
+        $sql="INSERT INTO `bom` (`no`, `bom_id`, `mName`, `qty`) VALUES (NULL, '".$bom_id."', '".$bom[0]."', '".$bom[1]."');";
+        mysqli_query( $con, $sql);
+      }
+      mysqli_close($con);
+      unset($_SESSION["bom"]);
+    }
+    ?>
+  </body>
+</html>
+<!--dan-->
