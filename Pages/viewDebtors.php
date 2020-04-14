c243339a4966e8e6111a226790c6655ae3039ff6
sithara <sitharasewwandi52@gmail.com>
2020-04-14 18:52:06 +0530

    add php for ViewDeptors

--- Pages/viewDebtors.php
+++ Pages/viewDebtors.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['eno'])){
+      header('Location:signIn.php');
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,3 +25,36 @@
           date
         </th>
     </tr>
+
+    <?php
+    $con=mysqli_connect("localhost","root","","galleon_lanka");
+    if(!$con)
+      {
+        die("cannot connect to DB server");
+      }
+
+      $sql="SELECT *,SUM(amount) AS tot FROM `debtors` GROUP BY `cno`";
+      $rowSQL=mysqli_query($con,$sql);
+      while($row=mysqli_fetch_array($rowSQL))
+      {
+echo"
+  <tr>
+      <td>
+          ".$row['cno']."
+      </td>
+      <td>
+          ".$row['tot']."
+      </td>
+      <td>
+          ".$row['date']."
+      </td>
+  </tr>
+";
+      }
+  ?>
+  </table>
+  </form>
+
+</body>
+</html>
+<!--sithara-->
