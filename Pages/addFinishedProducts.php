652ffe8e4faa6b0a2fbfe73ac43a3f98cab786e6
gima <gimantha.k98@gmail.com>
2020-04-16 00:43:41 +0530

    completed add interfaces

--- Pages/addFinishedProducts.php
+++ Pages/addFinishedProducts.php
@@ -8,6 +8,14 @@
   <head>
     <meta charset="utf-8">
     <title>Finished products</title>
+    <link rel="stylesheet" type="text/css" href="../Resources/CSS/normalize.css">
+    <link rel="stylesheet" type="text/css" href="../Resources/CSS/grid.css">
+    <link rel="stylesheet" type="text/css" href="../Resources/CSS/ionicons.min.css">
+    <link rel="stylesheet" type="text/css" href="../StyleSheets/MainStyles.css">
+    <link rel="stylesheet" type="text/css" href="../StyleSheets/ManageStyles.css">
+    <link rel="stylesheet" type="text/css" href="../StyleSheets/Select2Styles.css">
+    <!-- <link rel="stylesheet" type="text/css" href="../StyleSheets/Select3Styles.css"> -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400&display=swap" rel="stylesheet">
     <script type="text/javascript">
 
         /*function validateName(){
@@ -42,8 +50,28 @@
     </script>
   </head>
   <body>
-    <h1>add finished products</h1>
-    <form action="addFinishedProducts.php" method="post">
+  <header>
+        <div class="row">
+            <h1>Manufacturing Management System</h1>
+            <h3>Galleon Lanka PLC</h3>
+        </div>
+        <div class="nav">
+            <div class="row">
+                <div class="btn-navi"><i class="ion-navicon-round"></i></div>
+                <a href="empHome.php">
+                    <div class="btn-home"><i class="ion-home"></i><p>Home</p></div>
+                </a>
+                <a href="logout.php">
+                    <div class="btn-logout"><i class="ion-log-out"></i><p>Logout</p></div>
+                </a>
+                <a href="#"><div class="btn-account"><i class="ion-ios-person"></i><p>Account</p></div></a>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="row">
+            <div class="col span-2-of-2">
+            <form action="addFinishedProducts.php" method="post">
       <table>
         <tr>
           <td><label for="txtName">Name</label></td>
@@ -53,7 +81,7 @@
         <tr>
           <td><label for="lstBom">BOM ID</label></td>
           <td>
-            <select name="lstBom" id="lstBom">
+            <select name="lstBom" id="lstBom" style='width:200px;'>
               <option value='-----'>-----</option>
               <?php
                 $sql="SELECT DISTINCT `bom_id` FROM `bom`;";
@@ -78,7 +106,8 @@
           <td><input type="number" name="txtValue" id="txtValue" value="" min="0" step="0.01" required> </td>
         </tr>
         <tr>
-          <td colspan="2"><input type="submit" name="btnSubmit" id="btnSubmit" value="Add" onclick="Validate()"></td>
+          <td></td>
+          <td><input type="submit" name="btnSubmit" id="btnSubmit" value="Add" onclick="Validate()"></td>
         </tr>
       </table>
 
@@ -98,6 +127,13 @@
   	?>
 
     </form>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="row"><p> Copyright &copy; 2020 by Galleon Lanka PLC. All rights reserved.</p></div>
+        <div class="row"><p>Designed and Developed by DGS2</p></div>
+    </footer>
   </body>
 </html>
 
