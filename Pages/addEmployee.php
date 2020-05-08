47255ce86f049cdbc0be709c7f8265638903a9f0
gima <gimantha.k98@gmail.com>
2020-05-08 11:53:29 +0530

    added back buttons for Emp,Fp,viewstocks,profile pages

--- Pages/addEmployee.php
+++ Pages/addEmployee.php
@@ -72,9 +72,12 @@ if(!isset($_SESSION['eno'])){
         </div>
         <div class="nav">
             <div class="row">
-                <div class="btn-navi"><i class="ion-navicon-round"></i></div>
+                <!-- <div class="btn-navi"><i class="ion-navicon-round"></i></div> -->
                 <a href="empHome.php">
                     <div class="btn-home"><i class="ion-home"></i><p>Home</p></div>
+                </a>
+                <a href="viewEmployees.php">
+                    <div class="btn-back"><i class="ion-ios-arrow-back"></i><p>Back</p></div>
                 </a>
                 <a href="logout.php">
                     <div class="btn-logout"><i class="ion-log-out"></i><p>Logout</p></div>
