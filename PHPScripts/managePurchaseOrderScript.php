80ae9099907033959c9cbe7d05d44b2b3ca9324f
gima <gimantha.k98@gmail.com>
2020-05-09 20:57:55 +0530

    delete function to PO

--- PHPScripts/managePurchaseOrderScript.php
+++ PHPScripts/managePurchaseOrderScript.php
@@ -1,27 +1,27 @@
 <?php
   session_start();
-  $PO=$_SESSION['pOrder'];
-  if (isset($_POST['btnConfirm'])) {
-    // updating invoice records to approved
-    $con = mysqli_connect("localhost","root","","galleon_lanka");
+  $con = mysqli_connect("localhost","root","","galleon_lanka");
     if(!$con)
     {
       die("Error while connecting to database");
     }
+  $PO=$_SESSION['pOrder'];
+
+  if (isset($_POST['btnConfirm'])) {
     $sql="UPDATE `purchase_orders` SET `approvedBy` = '".$_SESSION['eno']."' WHERE `purchase_orders`.`po_no` = ".$PO.";";
     mysqli_query( $con,$sql);
-    
-    $sql1="SELECT * FROM `purchase_orders` where `po_no`=".$PO.";";
-    $rowSQL= mysqli_query( $con,$sql1);
-    $row = mysqli_fetch_array( $rowSQL );
-
-    // adding creditor records
-      $sql2="INSERT INTO `creditors` (`no`,`sid`, `amount`, `date`) VALUES (NULL,'".$row['sid']."', '".$row['amount']."',CURDATE());";
-      mysqli_query( $con,$sql2);
     }
-    mysqli_close($con);
     header('Location:../Pages/managePurchaseOrder.php');
+  
+  if (isset($_POST['btnDelete'])) {
+    $sql1="DELETE FROM `purchase_orders` WHERE `po_no`=$PO";
+    mysqli_query( $con,$sql1);
+    header('Location:../Pages/viewPurchaseOrders.php');
+  }
 
   if (isset($_POST['btnPrint'])) {
     header('Location:../Reports/printPurchaseOrder.php');
   }
+
+
+  mysqli_close($con);
